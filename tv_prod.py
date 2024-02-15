70169e7deb8b298509943c501190b999037e1463
julimika <julianameku@gmail.com>
2024-02-15 15:31:44 -0300

    validação carrosséis da home

--- tv_prod.py
+++ tv_prod.py
@@ -16,8 +16,30 @@ with sync_playwright() as p:
     time.sleep(5)
     Page.locator('xpath=//*[@id="App"]/section/div/div[1]/div').dblclick()
     time.sleep(10)
+    #validação dos carrosséis da home
+    Page.get_by_title("Canais Ao vivo")
+    print("Retornando ao vivo")
+    Page.get_by_title("Lançamentos")
+    print("Retornando lançamentos")
+    Page.get_by_title("Paramount")
+    print("Paramount retornando")
+    Page.get_by_title("Mais assistidos")
+    print("Mais assistidos retornando")
+    Page.get_by_title("CNN")
+    print("CNN Retornando")
+    Page.get_by_title("Awesomeness")   
+    print("Awesomeness retornando")
+    Page.get_by_title("Infantil")   
+    print("Infantil retornando")
+    Page.get_by_title("Sugestões para Você")   
+    print("Sugestões para Você retornando")
+    Page.get_by_title("Minha Lista")
+    print("Minha lista retornando")
     
-    # time.sleep(5)
-    # # page.getByLabel('profile-name').click('Mika PROD')
-    # # time.sleep(5)
-    # Page.get_by_role("p", name="Mika PROD").click()
+    Page.get_by_alt_text("Ícone do menu Filmes").click()
+    time.sleep(5)
+
+    
+    
+
+    
